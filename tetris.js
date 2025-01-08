28756e3ecfbcb1e9c225945e949633fe5a0cd542
FlackCode <fl4ckc0de@gmail.com>
2025-01-08 14:10:09 +0200

    Added block rotation

--- tetris.js
+++ tetris.js
@@ -12,7 +12,6 @@ function collide(arena, player) {
     const {matrix, pos} = player;
     for (let y = 0; y < matrix.length; ++y) {
         for (let x = 0; x < matrix[y].length; ++x) {
-            console.log("Checking");
             if (matrix[y][x] !== 0 && (arena[y + pos.y] && arena[y + pos.y][x + pos.x]) !== 0) {
                 return true;
             }
@@ -31,8 +30,9 @@ function createMatrix(width, height) {
 }
 
 function draw() {
-    context.fillStyle = "#000"
-    context.fillRect(0, 0, canvas.width, canvas.height)
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatrix(arena, {x: 0, y: 0});
     drawMatrix(player.matrix, player.pos);
 }
 
@@ -64,6 +64,13 @@ function merge(arena, player) {
     });
 }
 
+function playerMove(dir) {
+    player.pos.x += dir;
+    if (collide(arena, player)) {
+        player.pos.x -= dir;
+    }
+}
+
 function playerDrop() {
     player.pos.y++;
     if (collide(arena, player)) {
@@ -72,6 +79,24 @@ function playerDrop() {
         player.pos.y = 0;
     }
     dropCounter = 0;
+}
+
+function playerRotate(dir) {
+    rotate(player.matrix, dir);
+}
+
+function rotate(matrix, dir) {
+    for (let y = 0; y < matrix.length; ++y) {
+        for (let x = 0; x < y; ++x) {
+            [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
+        }
+    }
+
+    if (dir > 0) {
+        matrix.forEach(row => row.reverse());
+    } else {
+        matrix.reverse();
+    }
 }
 
 let dropCounter = 0;
@@ -91,11 +116,15 @@ function update(time = 0) {
 
 document.addEventListener("keydown", event => {
     if(event.key === "ArrowLeft") {
-        player.pos.x--;
-    } else if (event.key === "ArrowRight") {
-        player.pos.x++;
-    } else if(event.key === "ArrowDown") {
+        playerMove(-1);
+    } else if (event.code === "ArrowRight") {
+        playerMove(1);
+    } else if(event.code === "ArrowDown") {
         playerDrop();
+    } else if (event.code === "KeyQ") {
+        playerRotate(-1);
+    } else if (event.code === "KeyW") {
+        playerRotate(1);
     }
 })
 
