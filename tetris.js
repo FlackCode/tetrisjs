7fba86b2f82f37ca65fdd16096816892b702fc6c
FlackCode <fl4ckc0de@gmail.com>
2025-01-08 14:36:47 +0200

    Added colors to blocks

--- tetris.js
+++ tetris.js
@@ -8,6 +8,8 @@ const matrix = [
     [0, 1, 0],
 ]
 
+
+
 function collide(arena, player) {
     const {matrix, pos} = player;
     for (let y = 0; y < matrix.length; ++y) {
@@ -38,38 +40,38 @@ function createPiece(type) {
         ];
     } else if (type === "O") {
         return [
-            [1, 1],
-            [1, 1],
+            [2, 2],
+            [2, 2],
         ];
     } else if (type === "L") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [0, 1, 1],
+            [0, 3, 0],
+            [0, 3, 0],
+            [0, 3, 3],
         ];
     } else if (type === "J") {
         return [
-            [0, 1, 0],
-            [0, 1, 0],
-            [1, 1, 0],
+            [0, 4, 0],
+            [0, 4, 0],
+            [4, 4, 0],
         ];
     } else if (type === "I") {
         return [
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
-            [0, 1, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
+            [0, 5, 0, 0],
         ];
     } else if (type === "S") {
         return [
-            [0, 1, 1],
-            [1, 1, 0],
+            [0, 6, 6],
+            [6, 6, 0],
             [0, 0, 0],
         ];
     } else if (type === "Z") {
         return [
-            [1, 1, 0],
-            [0, 1, 1],
+            [7, 7, 0],
+            [0, 7, 7],
             [0, 0, 0],
         ];
     }
@@ -86,12 +88,23 @@ function drawMatrix(matrix, offset) {
     matrix.forEach((row, y) => {
         row.forEach((value, x) => {
             if (value !== 0) {
-                context.fillStyle = "red";
+                context.fillStyle = colors[value];
                 context.fillRect(x + offset.x, y + offset.y, 1, 1);
             }
         });
     });
 }
+
+const colors = [
+    null,
+    "red",
+    "blue",
+    "violet",
+    "green",
+    "purple",
+    "orange",
+    "pink"
+]
 
 const arena = createMatrix(12, 20);
 
