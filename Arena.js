807178174363a81e7256198de11046f605ba0bee
FlackCode <fl4ckc0de@gmail.com>
2025-01-08 18:48:05 +0200

    Cleaned up main into Player.js

---
+++ Arena.js
@@ -0,0 +1,23 @@
+export default class Arena {
+    constructor(width, height) {
+        const matrix = [];
+
+        this.matrix = matrix;
+    }
+
+    clear() {
+        this.matrix.forEach(row => row.fill(0));
+    }
+
+    merge(player) {
+
+    }
+
+    collide(player) {
+
+    }
+
+    sweep() {
+        
+    }
+}
--- main.js
+++ main.js
@@ -140,66 +140,6 @@ function merge(arena, player) {
     });
 }
 
-function playerMove(dir) {
-    player.pos.x += dir;
-    if (collide(arena, player)) {
-        player.pos.x -= dir;
-    }
-}
-
-function playerReset() {
-    const pieces = "ILJOTSZ";
-    player.matrix = createPiece(pieces[pieces.length * Math.random() | 0]);
-    player.pos.y = 0;
-    player.pos.x = (arena[0].length / 2 | 0) - (player.matrix[0].length / 2 | 0);
-    if (collide(arena, player)) {
-        arena.forEach(row => row.fill(0));
-        player.score = 0;
-        updateScore();
-    }
-}
-
-function playerDrop() {
-    player.pos.y++;
-    if (collide(arena, player)) {
-        player.pos.y--;
-        merge(arena, player);
-        playerReset();
-        arenaSweep();
-        updateScore();
-    }
-    dropCounter = 0;
-}
-
-function playerRotate(dir) {
-    const pos = player.pos.x;
-    let offset = 1;
-    rotate(player.matrix, dir);
-    while(collide(arena, player)) {
-        player.pos.x += offset;
-        offset = -(offset + (offset > 0 ? 1 : -1));
-        if (offset > player.matrix[0].length) {
-            rotate(player.matrix, -dir);
-            player.pos.x = pos;
-            return;
-        }
-    }
-}
-
-function rotate(matrix, dir) {
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < y; ++x) {
-            [matrix[x][y], matrix[y][x]] = [matrix[y][x], matrix[x][y]];
-        }
-    }
-
-    if (dir > 0) {
-        matrix.forEach(row => row.reverse());
-    } else {
-        matrix.reverse();
-    }
-}
-
 let dropCounter = 0;
 let dropInterval = 1000;
 let lastTime = 0;
