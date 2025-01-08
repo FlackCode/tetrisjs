d2b24742167f4bdc6b0d4ce672d29e9bdc3cb1fa
FlackCode <fl4ckc0de@gmail.com>
2025-01-08 18:59:23 +0200

    Moved arena functions into an Arena class

--- Arena.js
+++ Arena.js
@@ -1,7 +1,9 @@
 export default class Arena {
     constructor(width, height) {
         const matrix = [];
-
+        while(height--) {
+                matrix.push(new Array(width).fill(0));
+        }
         this.matrix = matrix;
     }
 
@@ -10,14 +12,45 @@ export default class Arena {
     }
 
     merge(player) {
-
+        player.matrix.forEach((row, y) => {
+            row.forEach((value, x) => {
+                if (value !== 0) {
+                    this[y + player.pos.y][x + player.pos.x] = value;
+                }
+            });
+        });
     }
 
     collide(player) {
+        const {matrix, pos} = player;
+        for (let y = 0; y < matrix.length; ++y) {
+            for (let x = 0; x < matrix[y].length; ++x) {
+                if (matrix[y][x] !== 0 && (this.matrix[y + pos.y] && this.matrix[y + pos.y][x + pos.x]) !== 0) {
+                    return true;
+                }
+            }
+        }
 
+        return false;
     }
 
     sweep() {
-        
+        let rowCount = 1;
+        let score = 0;
+        outer: for (let y = this.matrix.length - 1; y > 0; --y) {
+            for (let x = 0; x < this.matrix[y].length; ++x) {
+                if (this.matrix[y][x] === 0) {
+                    continue outer;
+                }
+            }
+
+            const row = this.matrix.splice(y, 1)[0].fill(0);
+            this.matrix.unshift(row);
+            ++y;
+
+            score += rowCount * 10;
+            rowCount *= 2;
+        }
+        return score;
     }
 }
--- main.js
+++ main.js
@@ -1,48 +1,3 @@
-const matrix = [
-    [0, 0, 0],
-    [1, 1, 1],
-    [0, 1, 0],
-]
-
-function arenaSweep() {
-    let rowCount = 1;
-    outer: for (let y = arena.length - 1; y > 0; --y) {
-        for (let x = 0; x < arena[y].length; ++x) {
-            if (arena[y][x] === 0) {
-                continue outer;
-            }
-        }
-
-        const row = arena.splice(y, 1)[0].fill(0);
-        arena.unshift(row);
-        ++y;
-
-        player.score += rowCount * 10;
-        rowCount *= 2;
-    }
-}
-
-function collide(arena, player) {
-    const {matrix, pos} = player;
-    for (let y = 0; y < matrix.length; ++y) {
-        for (let x = 0; x < matrix[y].length; ++x) {
-            if (matrix[y][x] !== 0 && (arena[y + pos.y] && arena[y + pos.y][x + pos.x]) !== 0) {
-                return true;
-            }
-        }
-    }
-
-    return false;
-}
-
-function createMatrix(width, height) {
-    const matrix = [];
-    while(height--) {
-        matrix.push(new Array(width).fill(0));
-    }
-    return matrix;
-}
-
 function createPiece(type) {
     if (type === "T") {
         return [
@@ -89,20 +44,6 @@ function createPiece(type) {
     }
 }
 
-function merge(arena, player) {
-    player.matrix.forEach((row, y) => {
-        row.forEach((value, x) => {
-            if (value !== 0) {
-                arena[y + player.pos.y][x + player.pos.x] = value;
-            }
-        });
-    });
-}
-
-
-
-
-
 document.addEventListener("keydown", event => {
     if(event.key === "ArrowLeft") {
         playerMove(-1);
@@ -116,6 +57,3 @@ document.addEventListener("keydown", event => {
         playerRotate(1);
     }
 })
-playerReset();
-updateScore();
-update();
